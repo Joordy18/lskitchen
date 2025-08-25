2e4f07d1be6064062379d8e76534dc11fbc6c5fe
Lucas Arcizet <164637154+Joordy18@users.noreply.github.com>
2025-08-25 10:22:22 +0200

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#0077B6] rounded-t-2xl text-white px-4 py-3 w-full">
+      <div className="max-w-screen-xl mx-auto flex items-center">
+        <span className="text-sm">Liens Utiles demander quoi mettre</span>
+      </div>
+    </footer>
+  );
+}
